7ce5e3ee9011b477d5373b4a0195e0ffe5c00b3f
calgreenex <carlos.alvarez@greenex.cl>
2026-01-20 13:11:30 -0300

    .

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -2,14 +2,36 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use App\Models\Worker;
-use App\Models\Field;
-use App\Models\TaskType;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Attendance extends Model
 {
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'company_id',
+        'worker_id',
+        'date',
+        'check_in_time',
+        'field_id',
+        'task_type_id',
+    ];
+
+    public function worker(): BelongsTo
+    {
+        return $this->belongsTo(Worker::class);
+    }
+
+    public function field(): BelongsTo
+    {
+        return $this->belongsTo(Field::class);
+    }
+
+    public function taskType(): BelongsTo
+    {
+        return $this->belongsTo(TaskType::class);
+    }
 }
